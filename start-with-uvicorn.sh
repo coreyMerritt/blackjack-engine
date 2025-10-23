41b6fb6c8031547b69e35ca74140cabb9fefa53d
Corey Merritt <coreyleemerritt@gmail.com>
2025-10-23 03:03:48 -0400

    Make startup script more robust

--- start-with-uvicorn.sh
+++ start-with-uvicorn.sh
@@ -1,15 +1,39 @@
 #!/usr/bin/env bash
 
-set -x
 set -e
 set -E
+set -x
 
-PYTHON_BIN=".venv/bin/python"
+# Ensure we're in the project dir
+script_dir="$(dirname "$(readlink -f "$0")")"
+cd "$script_dir"
 
-if [ ! -x "$PYTHON_BIN" ]; then
-  echo -e "\n\tError: Python not found in .venv. Is the venv set up?"
-  exit 1
+# Handle configs if necessary
+if [[ -d "./config" && -d "./config/model" ]]; then
+  if [[ ! -d "./config/prod" ]]; then
+    mkdir "./config/prod"
+  fi
+  if [[ ! -d "./config/backup" ]]; then
+    mkdir "./config/backup"
+  fi
+
+  if ! find "config/prod" -mindepth 1 | grep -q .; then
+    cp -r ./config/model/* ./config/prod/
+  fi
 fi
 
-PYTHONPATH=src "$PYTHON_BIN" -m uvicorn src.main:app --reload
+# Ensure we're in venv
+venv_existed=1
+if [[ ! -d .venv ]]; then
+  python3 -m venv .venv
+  venv_existed=0
+fi
+if [[ -f .venv/bin/activate ]]; then
+  source .venv/bin/activate
+fi
+if [[ ! $venv_existed -eq 1 ]]; then
+  pip install .
+fi
+
+PYTHONPATH=src python3 -m uvicorn src.main:app --reload
 
